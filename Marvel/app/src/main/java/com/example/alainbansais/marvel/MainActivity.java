5200afdb31a3e64d41adfd21acdd18fdc164b625
Alain BANSAIS <abansais@octo.com>
2016-06-01 17:45:19 +0200

    first page with recycler

--- Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
+++ Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
@@ -2,8 +2,31 @@ package com.example.alainbansais.marvel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "RecyclerViewExample";
+    private List<FeedItem> feedsList;
+    private RecyclerView mRecyclerView;
+    private MyRecyclerAdapter adapter;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
