2798fdd7edad18607621cb9e36a4f6ca9e66890d
Alain BANSAIS <abansais@octo.com>
2016-06-01 18:22:58 +0200

    new class callback empty

---
+++ Marvel/app/src/main/java/com/example/alainbansais/marvel/Callback.java
@@ -0,0 +1,16 @@
+package com.example.alainbansais.marvel;
+
+import android.os.AsyncTask;
+
+public class Callback extends AsyncTask<Void, Void, String>{
+
+    protected void onPreExecute() {
+
+
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        return null;
+    }
+}
--- Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
+++ Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
@@ -1,36 +1,32 @@
 package com.example.alainbansais.marvel;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "RecyclerViewExample";
-    private List<FeedItem> feedsList;
     private RecyclerView mRecyclerView;
-    private MyRecyclerAdapter adapter;
     private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize recycler view
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Downloading data from below url
+
     }
 }
