7ba0c91d153fd7d9740415d8a5cd6f98cd935fbd
Alain BANSAIS <abansais@octo.com>
2016-06-02 15:31:45 +0200

    Request available on textView

--- Marvel/app/src/main/java/com/example/alainbansais/marvel/Callback.java
+++ Marvel/app/src/main/java/com/example/alainbansais/marvel/Callback.java
@@ -1,16 +1,71 @@
 package com.example.alainbansais.marvel;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class Callback extends AsyncTask<Void, Void, String>{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+import static com.example.alainbansais.marvel.R.id.progressBar;
+import static com.example.alainbansais.marvel.R.id.responseView;
+
+public class Callback extends AsyncTask<Void, Void, String> {
+    protected MainActivity context;
+    private String API_URL = "http://gateway.marvel.com/v1/public/characters?";
+    private String TIME_STAMP = "1";
+    private String API_KEY = "e3abbc2f4d802fb69bc83f77a5463870";
+    private String HASH = "24807b3e495349dd682c2171df9bd96c";
+
+    public Callback(MainActivity currentContext){
+        context = currentContext;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
     protected void onPreExecute() {
-
-
+        super.onPreExecute();
+        context.findViewById(progressBar).setVisibility(VISIBLE);
     }
 
     @Override
     protected String doInBackground(Void... params) {
-        return null;
+
+        try {
+            URL url = new URL(API_URL + "ts=" + TIME_STAMP + "&apikey=" + API_KEY + "&hash=" + HASH);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                        urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
     }
+
+    @SuppressWarnings("ConstantConditions")
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        context.findViewById(progressBar).setVisibility(GONE);
+        TextView test = (TextView) context.findViewById(responseView);
+        test.setText(response);
+    }
+
 }
--- Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
+++ Marvel/app/src/main/java/com/example/alainbansais/marvel/MainActivity.java
@@ -2,31 +2,14 @@ package com.example.alainbansais.marvel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ProgressBar;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "RecyclerViewExample";
-    private RecyclerView mRecyclerView;
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Initialize recycler view
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        progressBar.setVisibility(View.VISIBLE);
-
-        // Downloading data from below url
-
+        Callback task = new Callback(this);
+        task.execute();
     }
 }
